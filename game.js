466c5ca0f62ebaeeace707fe971a27cf18861d5a
armaancha <145879027+armaancha@users.noreply.github.com>
2023-10-22 18:51:36 -0700

    Update game.js
    Added check for draw and added 2 new play modes, no player and single player. No player will allow 2 bots to play against each other at random, and single player will allow the user to face a bot.

--- game.js
+++ game.js
@@ -7,53 +7,77 @@ let x = [['-', '-', '-'],
 ['-', '-', '-']]
 
 printArray = () => {
-    x.forEach((item) => console.log(item[0] + " " + item[1] + " " + item[2]));
+  x.forEach((item) => console.log(item[0] + " " + item[1] + " " + item[2]));
 }
 
-playerMove = (p) => {
-    let r = prompt("Player " + p + " select row.");
-    let c = prompt("Player " + p + " select column.");
-    if (r > 2 || c > 2) {
-        console.log("Invalid Move.");
-        playerMove(p);
-        return;
-    }
-    if (x[r][c] === '-') {
-        x[r][c] = p === 1 ? 'X' : 'O'
-        printArray();
-        return;
-    }
+playerMove = (p, isBot) => {
+  let r = 0;
+  let c = 0;
+  if(!isBot) {
+    r = prompt("Player " + p + " select row.")-1;
+    c = prompt("Player " + p + " select column.")-1;
+  }
+  else {
+    r = Math.floor(Math.random() * 3);
+    c = Math.floor(Math.random() * 3);
+  }
+
+  if (r > 2 || c > 2) {
+    console.log("Invalid Move.");
+    playerMove(p, isBot);
+    return;
+  }
+  if (x[r][c] === '-') {
+    x[r][c] = p === 1 ? 'X' : 'O'
+    console.log("Player " + p + " move.");
+    printArray();
+    return;
+  }
+  if (!isBot) {
     console.log("Sorry, spot is taken.");
-    playerMove(p);
+  }
+  playerMove(p, isBot);
 }
 
 checkWin = (p) => {
-    let c = p === 1 ? 'X' : 'O'
-    if ((x[0][0] === c && x[0][1] === c && x[0][2] === c) ||
-        (x[1][0] === c && x[1][1] === c && x[1][2] === c) ||
-        (x[2][0] === c && x[2][1] === c && x[2][2] === c) ||
-        (x[0][0] === c && x[1][0] === c && x[2][0] === c) ||
-        (x[0][1] === c && x[1][1] === c && x[2][1] === c) ||
-        (x[0][2] === c && x[1][2] === c && x[2][2] === c) ||
-        (x[0][0] === c && x[1][1] === c && x[2][2] === c) ||
-        (x[0][2] === c && x[1][1] === c && x[2][0] === c)) {
-        console.log("Player " + p + " has won!");
-        return true;
+  let c = p === 1 ? 'X' : 'O'
+  if ((x[0][0] === c && x[0][1] === c && x[0][2] === c) ||
+    (x[1][0] === c && x[1][1] === c && x[1][2] === c) ||
+    (x[2][0] === c && x[2][1] === c && x[2][2] === c) ||
+    (x[0][0] === c && x[1][0] === c && x[2][0] === c) ||
+    (x[0][1] === c && x[1][1] === c && x[2][1] === c) ||
+    (x[0][2] === c && x[1][2] === c && x[2][2] === c) ||
+    (x[0][0] === c && x[1][1] === c && x[2][2] === c) ||
+    (x[0][2] === c && x[1][1] === c && x[2][0] === c)) {
+    console.log("Player " + p + " has won!");
+    return true;
+  }
+  return false;
+}
+
+checkDraw = () => {
+  for(let i=0; i<x.length; i++) {
+    for(let j=0; j<x[i].length; j++) {
+      if(x[i][j] === '-') {
+        return false;
+      }
     }
-    return false;
+  }
+  console.log("It's a draw!")
+  return true;
 }
 
 playGame = () => {
-    let win = false;
-    printArray();
-    while (!win) {
-        playerMove(1);
-        win = checkWin(1);
-        if (win) { break };
-        playerMove(2);
-        win = checkWin(2);
-        if (win) { break };
-    }
+  let option = prompt("0 for no player, 1 for singleplayer, 2 for multiplayer ");
+  let player1Bot = option === "0" ? true : false;
+  let player2Bot = option === "0" || "1" ? true : false;
+  printArray();
+  while (true) {
+    playerMove(1, player1Bot);
+    if(checkWin(1) || checkDraw()) { break };
+    playerMove(2, player2Bot);
+    if(checkWin(2) || checkDraw()) { break };
+  }
 }
 
 playGame()
